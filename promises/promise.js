5064d0a3b560f5c43e0e5b83b26408a7f956201d
Ronak Radadiya <ronakradadiya7@gmail.com>
2022-05-01 10:01:07 +0530

    Polyfill promise final version

---
+++ promises/promise.js
@@ -0,0 +1,100 @@
+// Async and sync promise with multiple then, catch and fianlly block
+class MyPromise {
+  resolvedData;
+  isResolved = false;
+  resolveChain = [];
+
+  rejectedData;
+  isRejected = false;
+  rejectChain = [];
+
+  constructor(executor) {
+    const resolve = (data) => {
+      this.isResolved = true;
+      this.resolvedData = data;
+
+      if (this.resolveChain.length) {
+        this.resolveChain.forEach(func => {
+          this.resolvedData = func(this.resolvedData);
+        })
+      }
+    }
+
+    const reject = (data) => {
+      this.isRejected = true;
+      this.rejectedData = data;
+
+      if (this.rejectChain.length) {
+        this.rejectChain.forEach(func => {
+          this.rejectedData = func(this.rejectedData);
+        })
+      }
+    }
+
+    executor(resolve, reject);
+  }
+
+  then(func) {
+    if (this.isResolved) {
+      this.resolvedData = func(this.resolvedData);
+    } else {
+      this.resolveChain.push(func);
+    }
+    return this;
+  }
+
+  catch(func) {
+    if (this.isRejected) {
+      this.rejectedData = func(this.rejectedData)
+    } else {
+      this.rejectChain.push(func);
+    }
+    return this;
+  }
+
+  finally(func) {
+    if (this.isResolved) {
+      this.resolvedData = func(this.resolvedData);
+    } else {
+      this.resolveChain.push(func);
+    }
+
+    if (this.isRejected) {
+      this.rejectedData = func(this.rejectedData)
+    } else {
+      this.rejectChain.push(func);
+    }
+  }
+}
+
+new MyPromise((resolve, reject) => {
+  // setTimeout(() => {
+  //   resolve(10);
+  // }, 1000)
+  reject('Something wrong...');
+  // resolve(10);
+}).then((data) => {
+  console.log('Inside then 1');
+  return data * 2;
+}).then((data) => {
+  console.log('Inside then 2');
+  return data * 2
+}).then((data) => {
+  console.log('Inside then 3');
+  console.log(data);
+  return data;
+})
+.catch(err => {
+  console.log('Inside catch 1');
+  return new Error(err);
+})
+.catch(err => {
+  console.log('Inside catch 2');
+  return err;
+})
+.catch(err => {
+  console.log('Inside catch 3');
+  console.log(err);
+  return err;
+})
+.finally(data => console.log(data));
